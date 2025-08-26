8c39dfb76beda6f2181e014ffb17f658aa50e122
sagar <sagarkundrapu234@gmail.com>
2025-08-26 17:16:22 +0530

    created backend  /submittodoitem route in the branch master_2

---
+++ ToDo page/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+db = client.todo_db
+collection = db.todo_items
+
+@app.route('/')
+def index():
+    return "modda gudu"
+
+@app.route('/submittodoitem', methods=['GET', 'POST'])
+def submit():
+    # data = request.get_json()
+    data = request.form.to_dict()
+    print(data)
+    collection.insert_one(data)
+    return "Item added successfully"
+
+
+if __name__ == '__main__':  
+    app.run(debug=True)
