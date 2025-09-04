59de290cd5e94ee12d32f0def314a4096c1d96e1
sagar <sagarkundrapu234@gmail.com>
2025-09-04 16:45:49 +0530

    Added /submit route in the frontend to forward data to server

--- Todo/frontend/app.py
+++ Todo/frontend/app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+from flask import request, redirect
+import requests
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def todo():
     return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form.to_dict()
+    requests.post('http://localhost:5000/submittodoitem', data=data)
+    return ""
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000, host='0.0.0.0')
